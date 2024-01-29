f9f497e9544335150e788fab7d5f2938b3a1874d
Luka <105901329+Luka-PG@users.noreply.github.com>
2024-01-29 11:15:10 +0100

    initial commit / project done

---
+++ src/__tests__/Bills.js
@@ -0,0 +1,165 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {screen, waitFor} from "@testing-library/dom"
+import userEvent from '@testing-library/user-event'
+import BillsUI from "../views/BillsUI.js"
+import { bills } from "../fixtures/bills.js"
+import { ROUTES, ROUTES_PATH} from "../constants/routes.js";
+import {localStorageMock} from "../__mocks__/localStorage.js";
+import Bills from '../containers/Bills.js'
+import mockStore from "../__mocks__/store"
+
+import router from "../app/Router.js";
+
+jest.mock("../app/store", () => mockStore)
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on Bills Page", () => {
+    test("Then bill icon in vertical layout should be highlighted", async () => {
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByTestId('icon-window'))
+      const windowIcon = screen.getByTestId('icon-window')
+      //to-do write expect expression
+
+    })
+    test("Then bills should be ordered from earliest to latest", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+      
+      const billsContainer = new Bills({
+        document, onNavigate, store: mockStore, localStorage: null
+      })
+      const result = await billsContainer.getBills()
+      const dates = result.map(bill => bill.date)
+
+      const antiChrono = (a, b) => ((a < b) ? 1 : -1)
+      const datesSorted = [...dates].sort(antiChrono)
+      expect(dates).toEqual(datesSorted)
+    })
+
+    test("Then it should fetch bills from mock API GET", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.append(root)
+      router()
+      window.onNavigate(ROUTES_PATH.Bills)
+      await waitFor(() => screen.getByText("Mes notes de frais"))
+      const mockedBill1Nom  = await screen.getByText("encore")
+      expect(mockedBill1Nom).toBeTruthy()
+      const mockedBill1Date  = await screen.getByText("4 Avr. 04")
+      expect(mockedBill1Date).toBeTruthy()
+    })
+  })
+
+  describe("When I click on new bill button", () => {
+    test("Then it should open a page to create a new bill", () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const billsContainer = new Bills({
+        document, onNavigate, store: mockStore, localStorage: null
+      })
+      document.body.innerHTML = BillsUI({ data: bills })
+      const handleClickButton = jest.fn((e) => billsContainer.handleClickNewBill())
+      const newBillButton = screen.getByTestId('btn-new-bill')
+      newBillButton.addEventListener('click', handleClickButton)
+      userEvent.click(newBillButton)
+      expect(handleClickButton).toHaveBeenCalled()
+      expect(screen.getByTestId('form-new-bill')).toBeTruthy()
+    })
+  })
+
+  describe("When I click on one of the eye icons", () => {
+    test("Then it should open the modal and show an image", async () => {
+      const onNavigate = (pathname) => {
+        document.body.innerHTML = ROUTES({ pathname })
+      }
+
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+
+      const billsContainer = new Bills({
+        document, onNavigate, store: mockStore, localStorage: null
+      })
+      document.body.innerHTML = BillsUI({ data: bills })
+      
+      const eyeButtons = screen.getAllByTestId("icon-eye")
+      const handleClickButton = jest.fn(() => billsContainer.handleClickIconEye(eyeButtons[1]))
+      eyeButtons[1].addEventListener('click', handleClickButton)
+      userEvent.click(eyeButtons[1])
+      expect(handleClickButton).toHaveBeenCalled()
+      expect(screen.getByTestId('modaleFile').getAttribute('class')).toContain('show')
+    })
+  })
+
+  describe("When an error occurs on API", () => {
+    beforeEach(() => {
+      jest.spyOn(mockStore, "bills")
+      Object.defineProperty(
+          window,
+          'localStorage',
+          { value: localStorageMock }
+      )
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee',
+        email: "a@a"
+      }))
+      const root = document.createElement("div")
+      root.setAttribute("id", "root")
+      document.body.appendChild(root)
+      router()
+    })
+    test("then it should fetch bills from an API and fails with 404 message error", async () => {
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 404"))
+          }
+        }})
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 404/)
+      expect(message).toBeTruthy()
+    })
+
+    test("Then it should fetch messages from an API and fails with 500 message error", async () => {
+
+      mockStore.bills.mockImplementationOnce(() => {
+        return {
+          list : () =>  {
+            return Promise.reject(new Error("Erreur 500"))
+          }
+        }})
+
+      window.onNavigate(ROUTES_PATH.Bills)
+      await new Promise(process.nextTick);
+      const message = await screen.getByText(/Erreur 500/)
+      expect(message).toBeTruthy()
+    })
+  })
+})
---
+++ src/containers/NewBill.js
@@ -0,0 +1,106 @@
+import { ROUTES_PATH } from '../constants/routes.js'
+import Logout from "./Logout.js"
+
+export default class NewBill {
+  constructor({ document, onNavigate, store, localStorage }) {
+    this.document = document
+    this.onNavigate = onNavigate
+    this.store = store
+    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+    formNewBill.addEventListener("submit", this.handleSubmit)
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+    file.addEventListener("change", this.handleChangeFile)
+    this.fileUrl = null
+    this.fileName = null
+    this.billId = null
+    new Logout({ document, localStorage, onNavigate })
+  }
+  handleChangeFile = e => {
+    e.preventDefault()
+    const fileField = this.document.querySelector(`input[data-testid="file"]`)
+    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const fileName = file.name
+    const fileNameArray = fileName.split(".")
+    const fileExtension = fileNameArray[fileNameArray.length-1].toLowerCase()
+    const acceptedExtensions = ['jpg', 'jpeg', 'png']
+    const errorMessageFile = this.document.querySelector(`span[data-testid="error-file"]`)
+    if (acceptedExtensions.some((extension) => fileExtension.includes(extension))){
+      if(fileField.classList.contains('red-border') && errorMessageFile.classList.contains('show')){
+        fileField.classList.replace('red-border', 'blue-border')
+        errorMessageFile.classList.replace('show', 'hide')
+      }
+      const formData = new FormData()
+      // @ts-ignore
+      const email = JSON.parse(localStorage.getItem("user")).email
+      formData.append('file', file)
+      formData.append('email', email)
+
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({fileUrl, key}) => {
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    } else {
+      fileField.value = ''
+      if(!fileField.classList.contains('red-border') && !errorMessageFile.classList.contains('show')){
+        fileField.classList.replace('blue-border', 'red-border')
+        errorMessageFile.classList.replace('hide', 'show')
+      }
+    }
+  }
+  handleSubmit = e => {
+    e.preventDefault()
+    const errorMessageSubmit = this.document.querySelector(`span[data-testid="error-submit"]`)
+    console.log(e.target.querySelector(`input[data-testid="datepicker"]`).value, this.document.querySelector(`input[data-testid="datepicker"]`).value)
+    // @ts-ignore
+    const email = JSON.parse(localStorage.getItem("user")).email
+    const bill = {
+      email,
+      type: this.document.querySelector(`select[data-testid="expense-type"]`).value,
+      name:  this.document.querySelector(`input[data-testid="expense-name"]`).value,
+      amount: parseInt(this.document.querySelector(`input[data-testid="amount"]`).value),
+      date:  this.document.querySelector(`input[data-testid="datepicker"]`).value,
+      vat: this.document.querySelector(`input[data-testid="vat"]`).value,
+      pct: parseInt(this.document.querySelector(`input[data-testid="pct"]`).value) || 20,
+      commentary: this.document.querySelector(`textarea[data-testid="commentary"]`).value,
+      fileUrl: this.fileUrl,
+      fileName: this.fileName,
+      status: 'pending'
+    }
+    const billValues = Object.values(bill)
+    console.log(billValues)
+    console.log(billValues.every((value) => value !== null))
+    if (billValues.every((value) => value !== null)){
+      if(errorMessageSubmit.classList.contains('show')){
+        errorMessageSubmit.classList.replace('show', 'hide')
+      }
+      this.updateBill(bill)
+      this.onNavigate(ROUTES_PATH['Bills'])
+    } else {
+      if(!errorMessageSubmit.classList.contains('show')){
+        errorMessageSubmit.classList.replace('hide', 'show')
+      }
+    }
+  }
+
+  // not need to cover this function by tests
+  updateBill = (bill) => {
+    if (this.store) {
+      this.store
+      .bills()
+      .update({data: JSON.stringify(bill), selector: this.billId})
+      .then(() => {
+        this.onNavigate(ROUTES_PATH['Bills'])
+      })
+      .catch(error => console.error(error))
+    }
+  }
+}
